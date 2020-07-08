aea00335a443f4bbdd224d0dd210341bd3b7efdc
prez13 <roger.wiss@gmail.com>
2020-07-08 17:46:04 +0200

    improvements for detecting age and age comm

--- boardgamegeek.py
+++ boardgamegeek.py
@@ -438,14 +438,14 @@ class BGG:
 
         try:
             d['age'] = soup.findAll("span",
-                               {"ng-if": "::geekitemctrl.geekitem.data.item.minage > 0"})[0].getText().split()[0][:-1]  # everything except last item (+ sign, e.g. 14+)
+                               {"ng-if": "::geekitemctrl.geekitem.data.item.minage > 0"})[0].getText().split()[0].replace('+','') 
         except:
             d['age'] = ''
 
         try:
             d['age_community'] = \
             soup.findAll("span",
-                         {"ng-bind-html": "geekitemctrl.geekitem.data.item.polls.playerage|to_trusted"})[0].getText()
+                         {"ng-bind-html": "geekitemctrl.geekitem.data.item.polls.playerage|to_trusted"})[0].getText().replace('+','')
         except:
             d['age_community'] = ''
 
